907ace5bb68ab848052f0eb4d9038298c9917c98
Anna Thornton <athornton@pivotal.io>
2016-11-03 15:03:28 -0400

    WIP; print out aws cli command before running
    Signed-off-by: Jacques Chester <jchester@pivotal.io>

--- tasks/launch-ami/run.rb
+++ tasks/launch-ami/run.rb
@@ -9,7 +9,6 @@ ami_image_id = ami_data['ImageId']
 
 puts 'Creating EC2 instance'
 
-# specify --instance-type
 create_instance_output = `aws ec2 run-instances --image-id #{ami_image_id} --subnet-id subnet-106cd948`
 puts create_instance_output
 
@@ -18,5 +17,6 @@ new_instance_id = new_instance_data['Instances'].first['InstanceId']
 
 puts "Instance #{new_instance_id} created, waiting for it to be running"
 
-#puts `aws ec2 describe-instance-status --instance-id #{new_instance_id}`
+puts "aws ec2 wait instance-running --instance-ids #{new_instance_id}"
+puts 'Waiting...'
 puts `aws ec2 wait instance-running --instance-ids #{new_instance_id}`
