8100dac9bfcfd2b1aecd7f0a2464b336f12fd0b0
Anna Thornton <athornton@pivotal.io>
2016-11-04 10:21:30 -0400

    WIP; misc changes
    - wait on system-status-ok
    - use the same EC2 size as production instances
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/launch-ami/run.rb
+++ tasks/launch-ami/run.rb
@@ -8,7 +8,7 @@ ami_image_id = ami_data['ImageId']
 
 puts 'Creating EC2 instance'
 
-create_instance_output = `aws ec2 run-instances --image-id #{ami_image_id} --subnet-id subnet-106cd948 --associate-public-ip-address`
+create_instance_output = `aws ec2 run-instances --image-id #{ami_image_id} --subnet-id subnet-106cd948 --instance-type c3.4xlarge --associate-public-ip-address`
 puts create_instance_output
 
 File.write('launched-instances/instance.json', create_instance_output)
@@ -16,8 +16,6 @@ File.write('launched-instances/instance.json', create_instance_output)
 new_instance_data = JSON.parse(create_instance_output)
 new_instance_id = new_instance_data['Instances'].first['InstanceId']
 
-puts "Instance #{new_instance_id} created, waiting for it to be running"
+puts "Instance #{new_instance_id} created, waiting for status: OK"
 
-puts "aws ec2 wait instance-running --instance-ids #{new_instance_id}"
-puts 'Waiting...'
-puts `aws ec2 wait instance-running --instance-ids #{new_instance_id}`
+system "aws ec2 wait system-status-ok --instance-ids #{new_instance_id}"
